fdf055354579b7ea7f19eaf9de7e6ee5fef8eea8
Per Rovegård <per@rovegard.se>
2021-06-30 13:37:47 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing.Imaging;
+using IronPdf;
+
+namespace ironpdf_convert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            License.LicenseKey = "";
+
+            Console.WriteLine("OS:     " + Environment.OSVersion);
+            Console.WriteLine(".NET:   " + Environment.Version);
+            Console.WriteLine("Iron:   " + VersionInfo.IronPdfAssemblyVersion);
+            Console.WriteLine("Deploy: " + Installation.ActualDeploymentPath);
+
+            var name = args[0];
+            var nameNoExt = name[..name.LastIndexOf(".")];
+            var pdf = new PdfDocument(name);
+            var images = pdf.ToBitmap();
+            for (var i = 0; i < images.Length; i++)
+            {
+                var imageName = $"{nameNoExt}_{i}.png";
+                Console.WriteLine($"Saving image to {imageName}");
+                images[i].Save(imageName, ImageFormat.Png);
+                images[i].Dispose();
+            }
+        }
+    }
+}
